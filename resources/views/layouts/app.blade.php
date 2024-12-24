e429158fd4e47c0629831eaeb863baaa77516853
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-24 12:47:24 +0100

    Worked on Product Create Form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,42 +1,4 @@
-{{--<!DOCTYPE html>--}}
-{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
-{{--    <head>--}}
-{{--        <meta charset="utf-8">--}}
-{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
-{{--        <meta name="csrf-token" content="{{ csrf_token() }}">--}}
-
-{{--        <title>{{ config('app.name', 'Laravel') }}</title>--}}
-
-{{--        <!-- Fonts -->--}}
-{{--        <link rel="preconnect" href="https://fonts.bunny.net">--}}
-{{--        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />--}}
-
-{{--        <!-- Scripts -->--}}
-{{--        @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
-{{--    </head>--}}
-{{--    <body class="font-sans antialiased">--}}
-{{--        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">--}}
-{{--            @include('layouts.navigation')--}}
-
-{{--            <!-- Page Heading -->--}}
-{{--            @isset($header)--}}
-{{--                <header class="bg-white dark:bg-gray-800 shadow">--}}
-{{--                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">--}}
-{{--                        {{ $header }}--}}
-{{--                    </div>--}}
-{{--                </header>--}}
-{{--            @endisset--}}
-
-{{--            <!-- Page Content -->--}}
-{{--            <main>--}}
-{{--                {{ $slot }}--}}
-{{--            </main>--}}
-{{--        </div>--}}
-{{--    </body>--}}
-{{--</html>--}}
-
-
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -61,15 +23,19 @@
 
     @include('layouts.footer')
 
-<!--jquery library js-->
-<script src=" {{ asset('assets/js/jquery-3.7.1.min.js') }}"></script>
-<!--bootstrap js-->
-<script src=" {{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
-<!--font-awesome js-->
-<script src=" {{ asset('assets/js/Font-Awesome.js') }}"></script>
+    <!--jquery library js-->
+    <script src=" {{ asset('assets/js/jquery-3.7.1.min.js') }}"></script>
+    <!--bootstrap js-->
+    <script src=" {{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <!--font-awesome js-->
+    <script src=" {{ asset('assets/js/Font-Awesome.js') }}"></script>
+    <!--plugins js-->
+    <script src=" {{ asset('assets/js/tinymce/tinymce.min.js') }}"></script>
 
-<!--main/custom js-->
-<script src=" {{ asset('assets/js/main.js') }}"></script>
+    <!--main/custom js-->
+    <script src=" {{ asset('assets/js/main.js') }}"></script>
+
+    {{ $scripts ?? '' }}
 
 </body>
 
