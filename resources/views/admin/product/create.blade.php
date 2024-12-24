328ec5c7ecd6e7367c5465148f0abd1ab19234d5
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-24 18:12:49 +0100

    Worked on Validating Form infromation

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -3,54 +3,72 @@
     <section class="wsus__product mt_145 pb_100">
         <div class="container">
 
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+
             <h4 class="pt-3 pb-3 text-primary">Dashboard</h4>
             <div  class="card">
-                <div class="card-header d-flex justify-content-between">
-                    <h5>Create Product</h5>
-                    <a href=" {{ route('dashboard') }} " class="btn btn-primary">Go Back</a>
-                </div>
-                <div  class="card-body">
-                    <div  class="form-group">
-                        <label for="" class="mt-2 mb-2">Images</label>
-                        <x-text-input type="file" class="form-control" name="images" />
+'                <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+'                    <div class="card-header d-flex justify-content-between">
+                        <h5>Create Product</h5>
+                        <a href=" {{ route('dashboard') }} " class="btn btn-primary">Go Back</a>
                     </div>
-                    <div  class="form-group">
-                        <label for="" class="mt-2 mb-2">Name</label>
-                        <x-text-input type="text" class="form-control" name="name" />
-                    </div>
-                    <div  class="form-group">
-                        <label for="" class="mt-2 mb-2">Price</label>
-                        <x-text-input type="text" class="form-control" name="price" />
-                    </div>
-                    <div  class="form-group">
-                        <label for="" class="mt-2 mb-2">Colors</label>
-                        <x-select-input name="colors" >
-                            <option value="">Select</option>
-                            <option value="black">Black</option>
-                            <option value="green">Green</option>
-                            <option value="red">Red</option>
-                            <option value="cyan">Cyan</option>
-                        </x-select-input>
-                    </div>
-                    <div  class="form-group">
-                        <label for="" class="mt-2 mb-2">Short Description</label>
-                        <x-text-input type="text" class="form-control" name="short_description" />
-                    </div>
-                    <div  class="form-group">
-                        <label for="" class="mt-2 mb-2">Qty</label>
-                        <x-text-input type="text" class="form-control" name="qty" />
-                    </div>
-                    <div  class="form-group">
-                        <label for="" class="mt-2 mb-2">Sku</label>
-                        <x-text-input type="text" class="form-control" name="sku" />
-                    </div>
-                    <div  class="form-group">
-                        <label for="" class="mt-2 mb-2">Description</label>
-                        <textarea name="description" id="editor" cols="30" rows="10"></textarea>
-                    </div>
+                    <div  class="card-body">
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Image</label>
+                            <x-text-input type="file" class="form-control" name="image" />
+                        </div>
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Images</label>
+                            <x-text-input type="file" class="form-control" name="images[]" multiple />
+                        </div>
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Name</label>
+                            <x-text-input type="text" class="form-control" name="name" />
+                        </div>
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Price</label>
+                            <x-text-input type="text" class="form-control" name="price" />
+                        </div>
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Colors</label>
+                            <x-select-input name="colors" multiple>
+                                <option value="">Select</option>
+                                <option value="black">Black</option>
+                                <option value="green">Green</option>
+                                <option value="red">Red</option>
+                                <option value="cyan">Cyan</option>
+                            </x-select-input>
+                        </div>
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Short Description</label>
+                            <x-text-input type="text" class="form-control" name="short_description" />
+                        </div>
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Qty</label>
+                            <x-text-input type="text" class="form-control" name="qty" />
+                        </div>
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Sku</label>
+                            <x-text-input type="text" class="form-control" name="sku" />
+                        </div>
+                        <div  class="form-group">
+                            <label for="" class="mt-2 mb-2">Description</label>
+                            <textarea name="description" id="editor" cols="30" rows="10"></textarea>
+                        </div>
 
-                    <x-primary-button> Submit </x-primary-button>
-                </div>
+                        <x-primary-button> Submit </x-primary-button>
+                    </div>
+                </form>
             </div>
 
         </div>
