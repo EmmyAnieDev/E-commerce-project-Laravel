cd13ccacf4c68fbdc2f61947259c0b7373b01a7d
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-24 10:04:53 +0100

    Worked on Admin Dashboard

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,12 +1,14 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        <h4 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Update Password') }}
-        </h2>
+        </h4>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
+
+        <hr>
     </header>
 
     <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
@@ -40,7 +42,7 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
+                    class="text-success font-medium text-sm"
                 >{{ __('Saved.') }}</p>
             @endif
         </div>
